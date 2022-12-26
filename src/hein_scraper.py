c31843f1da5cf69fd9919d884cd6a14c77148862
Alejandrina Gonzalez <alegre@stanford.edu>
2022-12-25 21:56:10 -0500

    fix import

--- src/hein_scraper.py
+++ src/hein_scraper.py
@@ -8,7 +8,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.wait import WebDriverWait
 from tqdm import tqdm
 
 import defaults
